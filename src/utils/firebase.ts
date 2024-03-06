8ac398ea54df5afaaccff88b9c4e008a30d260db
maxamed1st <maxamed1st@gmail.com>
2024-03-06 21:11:40 +0100

    configure firebase locally

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,28 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
 // Initialize Firebase
 const firebaseConfig = {
-
   apiKey: "AIzaSyANBrd_iUfb49EZ9VeThupptkqPHQPqf5o",
-
   authDomain: "turjum-cf6e9.firebaseapp.com",
-
   projectId: "turjum-cf6e9",
-
   storageBucket: "turjum-cf6e9.appspot.com",
-
   messagingSenderId: "725718374058",
-
   appId: "1:725718374058:web:50e19b72d07526150922d3"
-
 };
 
 
