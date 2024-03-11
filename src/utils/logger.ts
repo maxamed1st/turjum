74b281f2fe67740d17b1a3c2f151e91c807ebc1f
maxamed1st <maxamed1st@gmail.com>
2024-03-11 17:22:44 +0100

    log errors

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,7 @@
 export default function log(src: String, msg: any = "") {
   return console.log(new Date().toLocaleString("sv-sw"), src, "\n", msg);
 }
+export function logErr(src: string, err: any) {
+  err?.status && console.error(src, err.status);
+  console.error(src, err?.errors ? JSON.stringify(err.errors) : err);
+}
