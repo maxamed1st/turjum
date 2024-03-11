3821bcb8362d634604994760edc8eb6958f2aaf1
maxamed1st <maxamed1st@gmail.com>
2024-03-11 21:11:58 +0100

    implement rednering logic

---
+++ src/screens/translate/features/Document.tsx
@@ -0,0 +1,94 @@
+import Spinner from '@/components/Spinner';
+import { styles } from '@/components/Styles';
+import useUser from '@/store/useUser';
+import { db, uploadDocdocument } from '@/utils/firebase';
+import log from '@/utils/logger';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { addDoc, collection } from 'firebase/firestore';
+import { useAtomValue, useSetAtom } from 'jotai';
+import React, { useState } from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { jobDocument } from 'types';
+import { titleAtom, uriAtom } from '..';
+
+export default function TranslateDocument() {
+  const currentUser = useUser(state => state.currentUser);
+
+  const title = useAtomValue(titleAtom);
+  const setTitle = useSetAtom(titleAtom);
+  const uri = useAtomValue(uriAtom);
+
+  const [srcLang, setSrcLang] = useState("");
+  const [targetLang, setTargetLang] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUploadDoc = async () => {
+    try {
+      setIsLoading(true);
+      const uid = currentUser?.uid as string;
+      const path = await uploadDocdocument({ currentUser, title, uri }) as string;
+
+      const colRef = collection(db, "users", uid, "jobs");
+
+      const data: jobDocument = {
+        title,
+        path: path,
+        srcLang,
+        targetLang,
+        credit: 0
+      }
+
+      const result = await addDoc(colRef, data);
+      log("Translate", result);
+    }
+
+    catch (err) {
+      log("translate", err)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <SafeAreaView tw="flex-1 justify-between m-2">
+      <View tw="flex-row justify-between border p-3 rounded-md">
+        <Text tw="text-xs">{title}</Text>
+        <Ionicons name="close-sharp" size={18} onPress={() => setTitle("")} />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          autoCapitalize="none"
+          value={srcLang}
+          style={styles.textInput}
+          placeholder="source langauge..."
+          placeholderTextColor="#000"
+          onChangeText={(text) => setSrcLang(text.trim())}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          autoCapitalize="none"
+          value={targetLang}
+          style={styles.textInput}
+          placeholder="target langauge..."
+          placeholderTextColor="#000"
+          onChangeText={(text) => setTargetLang(text.trim())}
+        />
+      </View>
+
+      <Pressable
+        onPress={handleUploadDoc}
+        tw="w-full h-14 bg-black items-center justify-center p-3 rounded"
+      >
+        {isLoading ?
+          <Spinner />
+          :
+          <Text tw="text-white">Turjum</Text>
+        }
+      </Pressable>
+    </SafeAreaView >
+  )
+}
--- src/screens/translate/index.tsx
+++ src/screens/translate/index.tsx
@@ -1,8 +1,56 @@
+import { Ionicons } from '@expo/vector-icons';
+import { getDocumentAsync } from 'expo-document-picker';
+import { atom, useAtom } from 'jotai';
 import React from 'react';
-import TranslateDocument from './features/translateDocument';
+import { SafeAreaView, Text, View } from 'react-native';
+import CameraComponent from './features/Camera';
+import TranslateDocument from './features/Document';
+
+type renderingType = "none" | "document" | "camera";
+
+const renderingAtom = atom<renderingType>("none")
+
+export const titleAtom = atom("");
+export const uriAtom = atom("");
 
 export default function Translate() {
+  const [rendering, setRendering] = useAtom(renderingAtom);
+  const setTitle = useAtom(titleAtom)[1];
+  const setUri = useAtom(uriAtom)[1];
+
+  const handlePickDocument = async () => {
+    const doc = await getDocumentAsync();
+    const docName = doc.assets?.at(0)?.name as string;
+    const uri = doc.assets?.at(0)?.uri as string;
+
+    setTitle(docName);
+    setUri(uri);
+
+    setRendering("document");
+  }
+
   return (
-    <TranslateDocument />
+    <>
+      {rendering == "none" ?
+        <SafeAreaView tw="flex-1 items-center justify-evenly">
+          < View >
+            <Text tw="text-blue-600 text-center" onPress={handlePickDocument}>upload a document...</Text>
+          </View >
+
+          <View>
+            <Text>OR</Text>
+          </View>
+
+          <View>
+            <Ionicons name='camera' size={32} color="#f00" onPress={() => setRendering("camera")} />
+          </View>
+        </SafeAreaView >
+        :
+        rendering == "document" ?
+          <TranslateDocument />
+          :
+          <CameraComponent />
+      }
+    </>
   )
 }
