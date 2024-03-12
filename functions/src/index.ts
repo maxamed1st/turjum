7ce31ac250a0ed463f4e66e2fe2be5ae1cafe7c6
maxamed1st <maxamed1st@gmail.com>
2024-03-12 15:25:37 +0100

    rename functions and change path

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,13 +4,13 @@ import { onDocumentCreated, onDocumentWritten } from "firebase-functions/v2/fire
 
 admin.initializeApp();
 
-export const translation = onDocumentCreated("users/{userId}/", async (event) => {
+export const handleDocument = onDocumentCreated("users/{userId}/jobs", async (event) => {
   const data = event.data?.data();
   const id = event.data?.id;
 
   log(`${id} has been created with data:`, data);
 });
 
-export const rn = onDocumentWritten("users/{userId}//jobs/", async () => {
+export const handleImage = onDocumentWritten("users/{userId}/jobs", async () => {
   log(`new data:`);
 });
