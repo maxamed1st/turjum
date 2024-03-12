8b5a4fccae318f14a95becfb9c2206c13159c6ae
maxamed1st <maxamed1st@gmail.com>
2024-03-12 15:40:05 +0100

    render loading state for user feedback

--- src/screens/translate/index.tsx
+++ src/screens/translate/index.tsx
@@ -1,3 +1,4 @@
+import Spinner from '@/components/Spinner';
 import { Ionicons } from '@expo/vector-icons';
 import { getDocumentAsync } from 'expo-document-picker';
 import { atom, useAtom } from 'jotai';
@@ -26,8 +27,10 @@ export default function Translate() {
   const [rendering, setRendering] = useAtom(renderingAtom);
   const setTitle = useAtom(titleAtom)[1];
   const setUri = useAtom(uriAtom)[1];
+  const [loading, setLoading] = React.useState(false);
 
   const handlePickDocument = async () => {
+    setLoading(true);
     const doc = await getDocumentAsync();
     const docName = doc.assets?.at(0)?.name as string;
     const uri = doc.assets?.at(0)?.uri as string;
@@ -36,29 +39,34 @@ export default function Translate() {
     setUri(uri);
 
     setRendering("document");
+    setLoading(false);
   }
 
   return (
     <>
-      {rendering == "none" ?
-        <SafeAreaView tw="flex-1 items-center justify-evenly">
-          < View >
-            <Text tw="text-blue-600 text-center" onPress={handlePickDocument}>upload a document...</Text>
-          </View >
-
-          <View>
-            <Text>OR</Text>
-          </View>
-
-          <View>
-            <Ionicons name='camera' size={32} color="#f00" onPress={() => setRendering("camera")} />
-          </View>
-        </SafeAreaView >
-        :
-        rendering == "document" ?
-          <TranslateDocument />
+      {
+        loading ?
+          <Spinner />
           :
-          <CameraComponent />
+          rendering == "none" ?
+            <SafeAreaView tw="flex-1 items-center justify-evenly">
+              < View >
+                <Text tw="text-blue-600 text-center" onPress={handlePickDocument}>upload a document...</Text>
+              </View >
+
+              <View>
+                <Text>OR</Text>
+              </View>
+
+              <View>
+                <Ionicons name='camera' size={32} color="#f00" onPress={() => setRendering("camera")} />
+              </View>
+            </SafeAreaView >
+            :
+            rendering == "document" ?
+              <TranslateDocument />
+              :
+              <CameraComponent />
       }
     </>
   )
